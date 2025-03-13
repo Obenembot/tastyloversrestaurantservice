35a504dc372119e43da45110b33d62364e3c6c6d
Sampson Obenembot <sampson.obenembot@hellogroup.co.za>
2025-03-13 11:42:49 +0200

    added entities v2

--- src/main/java/za/co/moson/models/User.java
+++ src/main/java/za/co/moson/models/User.java
@@ -22,6 +22,9 @@ public class User {
     private String email;
 
     @Column(nullable = false)
+    private String mobileNumber;
+
+    @Column(nullable = false)
     private String password;
 
     @Column(nullable = false)

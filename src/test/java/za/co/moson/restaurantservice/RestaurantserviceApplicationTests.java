0f9dcb44138f91986f36cdceb03dc50617352c32
Sampson Obenembot <sampson.obenembot@hellogroup.co.za>
2025-03-13 08:28:19 +0200

    init

---
+++ src/test/java/za/co/moson/restaurantservice/RestaurantserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.moson.restaurantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

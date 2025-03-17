bf205e46237795db4be1f8d29955c6f605977014
Sampson Obenembot <sampson.obenembot@hellogroup.co.za>
2025-03-17 16:19:53 +0200

    remove unit test

--- src/test/java/za/co/moson/RestaurantServiceApplicationTests.java
+++ src/test/java/za/co/moson/RestaurantServiceApplicationTests.java
@@ -3,10 +3,10 @@ package za.co.moson;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RestaurantServiceApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
